095531919a42428052fb992c9404fe0b1aae6bbf
Philipp Gackstatter <philipp.gackstatter@iota.org>
2022-09-30 08:37:13 +0200

    Allow normal compilation of iota.rs to succeed

--- src/node_manager/http_client.rs
+++ src/node_manager/http_client.rs
@@ -70,8 +70,11 @@ impl HttpClient {
             request_builder = request_builder.timeout(_timeout);
         }
         
+        #[cfg(target_family = "wasm")]
         let start_time = instant::Instant::now();
-        
+        #[cfg(not(target_family = "wasm"))]
+        let start_time = std::time::Instant::now();
+
         let resp = request_builder.send().await?;
         log::debug!(
             "GET: {:?} ms for {} {}",
